1bbe4ae9f86215952099dd5159c89984155bb4dd
Arie van Luttikhuizen <aluttik@gmail.com>
2018-11-17 15:07:22 -0800

    The name "kitsune" was taken on pypi

--- kitsune/__init__.py
+++ kitsune/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__title__ = "kitsune"
+__title__ = "python-kitsune"
 __summary__ = "A prettier way to tail multiple files."
 __url__ = "https://github.com/aluttik/kitsune"
 

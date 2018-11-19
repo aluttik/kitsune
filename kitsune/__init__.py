43824cccf46f433a71b30a7febc0e3500b831067
Arie van Luttikhuizen <aluttik@gmail.com>
2018-11-18 21:49:22 -0800

    Increased version to 0.1.4

--- kitsune/__init__.py
+++ kitsune/__init__.py
@@ -7,7 +7,7 @@ __title__ = "python-kitsune"
 __summary__ = "A prettier way to tail multiple files."
 __url__ = "https://github.com/aluttik/kitsune"
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 __author__ = "Arie van Luttikhuizen"
 __email__ = "aluttik@gmail.com"
